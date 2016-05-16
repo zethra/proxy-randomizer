29bb3fcd77d89468a6aa0f40fa64e8aa52bf1335
zethra <jediben97@gmail.com>
2016-05-16 16:30:40 -0400

    Added terminate instance method and create instance methods

--- rndprox/main.py
+++ rndprox/main.py
@@ -2,20 +2,73 @@ import boto3
 
 
 def main():
-    list_instances()
+    # terminate_instance()
+    create_instance()
+    # list_instances()
 
 
 def create_instance():
+    client = boto3.client('ec2', region_name='us-east-1')
+    reservation = client.run_instances(
+            ImageId='ami-fce3c696',
+            MinCount=1,
+            MaxCount=1,
+            KeyName='proxy-manager',
+            InstanceType='t2.nano',
+            Placement={
+                'AvailabilityZone': 'us-east-1a'
+            }
+    )
+    instance_id = reservation['Instances'][0]['InstanceId']
+    while True:
+        states = client.describe_instance_status(InstanceIds=[instance_id])['InstanceStatuses']
+        if len(states) < 1:
+            print("No instances returned")
+            return
+        state = states[0]['InstanceState']
+        if state == 'running':
+            break
+    client.create_tags(
+            Resources=[instance_id],
+            Tags=[{
+                'Key': 'Name',
+                'Value': 'proxy-manager'
+            }]
+    )
+
+
+def terminate_instance():
     ec2 = boto3.resource('ec2', region_name='us-east-1')
-    ec2.create_instances()
+    client = boto3.client('ec2', region_name='us-east-1')
+    for i in ec2.instances.all():
+        if i.tags is None:
+            continue
+        for t in i.tags:
+            if t['Key'] == 'Name' and t['Value'] == 'proxy-manager' and i.state['Name'] == 'running':
+                print("Terminating: {0} / {1} {2} ({3}) - [{4}]".format(
+                        'us-east-1', i.id, t['Value'], i.instance_type, i.state['Name']
+                ))
+                client.stop_instances(InstanceIds=[i.id])
+                client.terminate_instances(InstanceIds=[str(i.id)])
+                break
+
+
+def get_image_id():
+    ec2 = boto3.resource('ec2', region_name='us-east-1')
+    return ec2.describe_images(Filters=[{'name': 'Ubuntu', 'architecture': 'x86_64'}])
 
 
 def list_running_instances():
     ec2 = boto3.resource('ec2', region_name='us-east-1')
-    instances = ec2.instances.filter(
-            Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-    for instance in instances:
-        print(instance.id, instance.instance_type)
+    for i in ec2.instances.all():
+        if i.tags is None:
+            print('No tags')
+            continue
+        for t in i.tags:
+            if t['Key'] == 'Name' and i.state['Name'] == 'running':
+                print("{0} / {1} {2} ({3}) - [{4}]".format(
+                        'us-east-1', i.id, t['Value'], i.instance_type, i.state['Name']
+                ))
 
 
 def list_instances():
